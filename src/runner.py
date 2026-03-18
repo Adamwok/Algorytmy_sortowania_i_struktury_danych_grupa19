4a672607af884c1d4d881ccbd0253f3b22ddbf36
Adam <adamekwojt08@gmail.com>
2026-03-18 21:45:58 +0100

    Dodanie ciągów wejściowych i wyjściowych

--- src/runner.py
+++ src/runner.py
@@ -1,10 +1,5 @@
 from src.generators import DataGenerator
-from src.algorithms import BubbleSort
-from src.algorithms import HeapSort
-from src.algorithms import MergeSort
-from src.algorithms import QuickSortIter
-from src.algorithms import QuickSortRec
-from src.algorithms import ShellSort
+from src.algorithms import BubbleSort, HeapSort, MergeSort, QuickSortIter, QuickSortRec, ShellSort
 from src.utils import CSVExporter
 
 class BenchmarkRunner:
@@ -12,35 +7,41 @@ class BenchmarkRunner:
         self.exporter = CSVExporter(results_path)
         self.n_values = n_values
         self.iterations = iterations
-
         self.algorithms = [
             ("BubbleSort", BubbleSort()),
             ("HeapSort", HeapSort()),
             ("MergeSort", MergeSort()),
             ("QuickSortIter", QuickSortIter()),
             ("QuickSortRec", QuickSortRec()),
-            ("ShellSort", ShellSort())
+            ("ShellSort", ShellSort()),
         ]
-    
+
+    def _format_sequence(self, data: list) -> str:
+        if len(data) <= 6:
+            return str(data)
+        return f"[{data[0]}, {data[1]}, {data[2]}, ..., {data[-3]}, {data[-2]}, {data[-1]}]"
+
     def execute_all(self):
         for n in self.n_values:
-            print(f"Generowanie i sortowanie dla n = {n}...")
-
             datasets = DataGenerator.generate_datasets(n, self.iterations)
-
             for algo_name, sorter in self.algorithms:
                 for shape, arrays in datasets.items():
                     for i, data in enumerate(arrays, start=1):
-                        sorted_data = sorter.run_sort(data)
-                        if sorted_data is not None:
-                            is_sorted = all(sorted_data[k] <= sorted_data[k+1] for k in range(len(sorted_data)-1))
+                        input_str = self._format_sequence(data)
+                        
+                        sorted_data = sorter.run_sort(data.copy())
+                        output_str = self._format_sequence(sorted_data)
+                        
+                        is_sorted = all(sorted_data[k] <= sorted_data[k+1] for k in range(len(sorted_data)-1))
                         custom_data_str = str(sorter.custom_metrics) if sorter.custom_metrics else "Brak"
-
+                        
                         self.exporter.append_result(
                             algorithm=algo_name,
                             n=n,
                             shape=shape,
                             iteration=i,
+                            input_seq=input_str,
+                            output_seq=output_str,
                             is_sorted=is_sorted,
                             time_val=sorter.execution_time,
                             comp=sorter.comparisons,

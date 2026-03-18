f98137c400dd831589c1adff504e6939d378b2a9
Adam <adamekwojt08@gmail.com>
2026-03-18 12:39:09 +0100

    Implementacja Merge Sort

---
+++ src/algorithms/merge_sort.py
@@ -0,0 +1,45 @@
+from .base import BaseSort
+
+class MergeSort(BaseSort):
+    def sort(self, data: list) -> list:
+        self.custom_metrics['merges'] = 0
+        self._merge_sort(data, 0, len(data) - 1)
+
+    def _merge_sort(self, data: list, left: int, right: int):
+        if left < right:
+            mid = (left + right) // 2
+            self._merge_sort(data, left, mid)
+            self._merge_sort(data, mid + 1, right)
+            self._merge(data, left, mid, right)
+        
+    def _merge(self, data: list, left: int, mid: int, right: int):
+        self.custom_metrics['merges'] += 1
+
+        left_part = data[left:mid + 1]
+        right_part = data[mid + 1:right + 1]
+
+        i = 0
+        j = 0
+        k = left
+
+        while i < len(left_part) and j < len(right_part):
+            self.comparisons += 1
+            if left_part[i] <= right_part[j]:
+                data[k] = left_part[i]
+                i += 1
+            else:
+                data[k] = right_part[j]
+                j += 1
+            k += 1
+
+        while i < len(left_part):
+            data[k] = left_part[i]
+            i += 1
+            k += 1
+
+        while j < len(right_part):
+            data[k] = right_part[j]
+            j += 1
+            k += 1
+            
+        

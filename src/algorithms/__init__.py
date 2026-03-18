8d7c5392c6fd6e1f76d08c0289671da246ab136c
Adam <adamekwojt08@gmail.com>
2026-03-18 14:16:10 +0100

    Testowa wersja programu

---
+++ src/algorithms/__init__.py
@@ -0,0 +1,7 @@
+from .base import BaseSort
+from .bubble_sort import BubbleSort
+from .heap_sort import HeapSort
+from .merge_sort import MergeSort
+from .quick_sort_iter import QuickSortIter
+from .quick_sort_rec import QuickSortRec
+from .shell_sort import ShellSort

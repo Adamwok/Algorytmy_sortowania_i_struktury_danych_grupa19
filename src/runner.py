af6d33ac70c5a6d607c92718fdf6782c23d40a1f
Adam <adamekwojt08@gmail.com>
2026-03-18 22:14:35 +0100

    Zmiana na całe ciągi

--- src/runner.py
+++ src/runner.py
@@ -16,18 +16,6 @@ class BenchmarkRunner:
             ("ShellSort", ShellSort()),
         ]
 
-    def _format_sequence(self, data: list) -> str:
-        if len(data) <= 100:
-            return str(data)
-        
-        front = data[:10]
-        back = data[-10:]
-        
-        front_str = ", ".join(map(str, front))
-        back_str = ", ".join(map(str, back))
-        
-        return f"[{front_str}, ..., {back_str}]"
-
     def execute_all(self):
         for n in self.n_values:
             print(f"Generowanie ciągów i sortowanie dla n = {n}...")
@@ -35,10 +23,10 @@ class BenchmarkRunner:
             for algo_name, sorter in self.algorithms:
                 for shape, arrays in datasets.items():
                     for i, data in enumerate(arrays, start=1):
-                        input_str = self._format_sequence(data)
+                        input_str = str(data)
                         
                         sorted_data = sorter.run_sort(data.copy())
-                        output_str = self._format_sequence(sorted_data)
+                        output_str = str(sorted_data)
                         
                         is_sorted = all(sorted_data[k] <= sorted_data[k+1] for k in range(len(sorted_data)-1))
                         custom_data_str = str(sorter.custom_metrics) if sorter.custom_metrics else "Brak"
